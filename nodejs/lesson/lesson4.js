c7d1d91cc41629d62a72a8d881c62ea2c1d5f229
xyjslxh <xyjslxh@163.com>
2017-07-18 16:48:51 +0800

    commit

--- nodejs/lesson/lesson4.js
+++ nodejs/lesson/lesson4.js
@@ -21,7 +21,7 @@ superagent.get(cnodeUrl).end(function(err, res) {
     console.log(topicUrls);
     var ep = new eventproxy();
     ep.after('topic_html', topicUrls.length, function(topics) {
-        topics = topics.map(function(index, topicPair) {
+        topics = topics.map(function(topicPair) {
             var topicUrl = topicPair[0];
             var topicHtml = topicPair[1];
             var $ = cheerio.load(topicHtml);
@@ -33,7 +33,7 @@ superagent.get(cnodeUrl).end(function(err, res) {
         });
         console.log('final');
         console.log(topics);
-    })
+    });
     topicUrls.forEach(function(topicUrl) {
         superagent.get(topicUrl)
             .end(function(err, res) {

b2f9a608afac268d020a73d58b34c2ccf1bdce4b
xyjslxh <xyjslxh@163.com>
2017-07-21 19:06:46 +0800

    commit

---
+++ nodejs/http/call_apply_extend.js
@@ -0,0 +1,13 @@
+function Pet(words){
+	this.words = words;
+	this.speak=function(){
+		console.log(this.words);
+	}
+}
+
+function Dog(words){
+	Pet.call(this, words);
+}
+
+var dog = new Dog('wang');
+dog.speak();

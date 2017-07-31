cc0fd31783cee7fc67ab07e2a44a660a58094909
xyjslxh <xyjslxh@163.com>
2017-07-31 10:51:07 +0800

    commit

--- nodejs/chatrooms/public/js/chat_ui.js
+++
@@ -1,7 +0,0 @@
-function divEscapedContentElemnet(message) {
-    return $('<div></div>').text(message);
-}
-
-function divSystemContentElement(message) {
-    return $('<div></div>').html('<i>' + message + '</i>');
-}
